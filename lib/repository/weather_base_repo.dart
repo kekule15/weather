2e0836339dfcdd0feac1d3ba97b6e975b56a5e50
Augustus <onyekachiaugustus111@gmail.com>
2024-03-13 16:32:05 +0100

    added API repository files for getting weather data using coordinates

--- lib/repository/weather_base_repo.dart
+++ lib/repository/weather_base_repo.dart
@@ -1,10 +1,10 @@
-
-
+import 'package:weather/model/user_location_weather_response_model.dart';
 import 'package:weather/model/weather_response_model.dart';
 
 abstract class BaseWeatherRepository {
+  Future<WeatherResponseModel?> getCityWeatherData(
+      {required String city, required String units, required String appId});
 
-  Future<WeatherResponseModel?> getCityWeatherData({required String city, required String units, required String appId});
-
-
+       Future<UserLocationResponseModel?> getWeatherByCoordinate(
+      {required double lat, required double lon, required String appId});
 }
--- lib/repository/weather_repository.dart
+++ lib/repository/weather_repository.dart
@@ -1,27 +1,48 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'package:weather/http/api_manager.dart';
+import 'package:weather/model/user_location_weather_response_model.dart';
 import 'package:weather/model/weather_response_model.dart';
 
- class WeatherRepository extends ApiManager {
+class WeatherRepository extends ApiManager {
   final Ref reader;
-  WeatherRepository(this.reader)  : super(reader);
+  WeatherRepository(this.reader) : super(reader);
   final getCityWeatherDataRoute = '/data/2.5/forecast/daily?';
+  final getWeatherByCoodinateRoute = '/data/2.5/weather?';
 
   Future<WeatherResponseModel?> getCityWeatherData(
       {required String city,
       required String units,
       required String appId}) async {
-    final params = {"q": city, "appid": appId, "units": units,};
+    final params = {
+      "q": city,
+      "appid": appId,
+      "units": units,
+    };
 
-    final response = await getHttp(
-      getCityWeatherDataRoute,
-      params: params
-    );
+    final response = await getHttp(getCityWeatherDataRoute, params: params);
     var data = response.data;
 
     if (response.statusCode == 200) {
       return WeatherResponseModel.fromJson(data);
+    } else {
+      return null;
+    }
+  }
+
+  Future<UserLocationResponseModel?> getWeatherByCoordinate(
+      {required double lat, required double lon, required String appId}) async {
+    final params = {
+      "lat": lat,
+      "lon": lon,
+      "appid": appId,
+    };
+
+    final response = await getHttp(getWeatherByCoodinateRoute, params: params);
+    var data = response.data;
+
+    if (response.statusCode == 200) {
+      return UserLocationResponseModel.fromJson(data);
     } else {
       return null;
     }
